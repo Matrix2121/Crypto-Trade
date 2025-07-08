1bb7454838c38e45a6bfa4136883e3c33d2de044
Matrix2121 <vasko10.vs38@gmail.com>
2025-07-08 18:16:02 +0300

    initialization

---
+++ backend/src/test/java/com/matrix2121/cryptotrader/CryptotraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix2121.cryptotrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptotraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
