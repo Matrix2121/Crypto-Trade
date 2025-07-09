634436788746b0ee3811426c8d9bcfdcc2888b64
Matrix2121 <vasko10.vs38@gmail.com>
2025-07-09 17:03:50 +0300

    holdings controller implemetation

---
+++ backend/src/main/java/com/matrix2121/cryptotrade/holdings/HoldingsController.java
@@ -0,0 +1,19 @@
+package com.matrix2121.cryptotrade.holdings;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/holdings")
+public class HoldingsController {
+
+    @Autowired
+    private HoldingsService service;
+
+    @GetMapping("/{userId}")
+    public List<HoldingsDto> getHoldings(@PathVariable Long userId) {
+        return service.getHoldingsByUserId(userId);
+    }
+}
