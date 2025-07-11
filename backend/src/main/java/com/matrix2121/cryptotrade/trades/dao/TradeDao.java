15b9c1cdab773c2b97e2871ddf2b89619f77bfc5
Matrix2121 <vasko10.vs38@gmail.com>
2025-07-11 12:49:55 +0300

    dao interface fix so it reflects the changes in service

--- backend/src/main/java/com/matrix2121/cryptotrade/trades/dao/TradeDao.java
+++ backend/src/main/java/com/matrix2121/cryptotrade/trades/dao/TradeDao.java
@@ -2,10 +2,9 @@ package com.matrix2121.cryptotrade.trades.dao;
 
 import java.math.BigDecimal;
 
-import com.matrix2121.cryptotrade.portfolio.AssetDto;
 import com.matrix2121.cryptotrade.trades.TradeDto;
 
 public interface TradeDao {
-    public TradeDto sellCrypto(Long userId, AssetDto assetDto, BigDecimal unitPrice);
-    public TradeDto buyCrypto(Long userId, AssetDto assetDto, BigDecimal unitPrice);
+    public TradeDto sellCrypto(Long userId, String cryptoCode, BigDecimal cryptoAmount, BigDecimal unitPrice);
+    public TradeDto buyCrypto(Long userId, String cryptoCode, BigDecimal cryptoAmount, BigDecimal unitPrice);
 }
