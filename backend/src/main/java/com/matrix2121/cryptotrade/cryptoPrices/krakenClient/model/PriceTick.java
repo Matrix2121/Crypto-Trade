320ad0676b199ac19ec3e81b79e9b74ca953b39a
Matrix2121 <vasko10.vs38@gmail.com>
2025-07-12 16:06:49 +0300

    websocket client side to kraken established

---
+++ backend/src/main/java/com/matrix2121/cryptotrade/cryptoPrices/krakenClient/model/PriceTick.java
@@ -0,0 +1,12 @@
+package com.matrix2121.cryptotrade.cryptoPrices.krakenClient.model;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record PriceTick(
+        String symbol,
+        BigDecimal ask,
+        BigDecimal bid,
+        BigDecimal last,
+        Instant timestamp) {
+}
